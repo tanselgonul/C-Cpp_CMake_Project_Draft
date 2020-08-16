84d80fb592a9be2ada05904eda7f8a40c4cc07cf
tanselgonul <tansel_gonul@hotmail.com>
2020-08-16 05:24:09 +0300

    first draft

---
+++ app/new_app/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <include/test.h>
+
+
+int main() {
+    print();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ libs/new_lib/src/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/test.h"
+
+int print() {
+    std::cout << "Hello, World! from library" << std::endl;
+    return 0;
+}
